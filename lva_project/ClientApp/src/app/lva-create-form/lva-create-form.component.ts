57491d0290f5106ab93a5b96cc81b199698c556d
fgreif <flo.g95@gmail.com>
2022-05-29 22:28:09 +0200

    added EnhancedFormgroup, added AddLva

--- lva_project/ClientApp/src/app/lva-create-form/lva-create-form.component.ts
+++ lva_project/ClientApp/src/app/lva-create-form/lva-create-form.component.ts
@@ -1,89 +1,60 @@
-import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ConfigService } from '../config.service';
+import { EnhancedFormControl, EnhancedFormGroup } from '../enhancedformgroup';
+
+import { LvaService } from '../lva.service';
+import { Lva } from '../models/lva';
 
 @Component({
   selector: 'app-lva-create-form',
   templateUrl: './lva-create-form.component.html',
   styleUrls: ['./lva-create-form.component.css']
 })
-export class LvaCreateFormComponent {
-  addressForm = this.fb.group({
-    lvaName: [null],
-    lvaTeacher: [null, Validators.required],
-    lvaNumber: [null, Validators.required],
-    lvaType: [null, Validators.required],
-    lvaRoom: [null],
-    lvaInstitute: [null, Validators.required],
-    lvaExam: [null, Validators.required],
-    lvaEcts: [null]
-  });
+export class LvaCreateFormComponent implements OnInit{ 
+  addressForm = EnhancedFormGroup.create({
+    lvaName: new EnhancedFormControl<string>(),
+    lvaTeacher: new EnhancedFormControl<string>(),
+    lvaNumber: new EnhancedFormControl<number>(),
+    lvaType: new EnhancedFormControl<number>(),
+    lvaRoom: new EnhancedFormControl<string>(),
+    lvaInstitute: new EnhancedFormControl<string>(),
+    lvaExam: new EnhancedFormControl<Date | null>(),
+    lvaEcts: new EnhancedFormControl<number>(),
+  })
+  lva = new Lva();
 
+  constructor(private _fb: FormBuilder, private readonly _lvaService: LvaService) {}
 
-  states = [
-    {name: 'Alabama', abbreviation: 'AL'},
-    {name: 'Alaska', abbreviation: 'AK'},
-    {name: 'American Samoa', abbreviation: 'AS'},
-    {name: 'Arizona', abbreviation: 'AZ'},
-    {name: 'Arkansas', abbreviation: 'AR'},
-    {name: 'California', abbreviation: 'CA'},
-    {name: 'Colorado', abbreviation: 'CO'},
-    {name: 'Connecticut', abbreviation: 'CT'},
-    {name: 'Delaware', abbreviation: 'DE'},
-    {name: 'District Of Columbia', abbreviation: 'DC'},
-    {name: 'Federated States Of Micronesia', abbreviation: 'FM'},
-    {name: 'Florida', abbreviation: 'FL'},
-    {name: 'Georgia', abbreviation: 'GA'},
-    {name: 'Guam', abbreviation: 'GU'},
-    {name: 'Hawaii', abbreviation: 'HI'},
-    {name: 'Idaho', abbreviation: 'ID'},
-    {name: 'Illinois', abbreviation: 'IL'},
-    {name: 'Indiana', abbreviation: 'IN'},
-    {name: 'Iowa', abbreviation: 'IA'},
-    {name: 'Kansas', abbreviation: 'KS'},
-    {name: 'Kentucky', abbreviation: 'KY'},
-    {name: 'Louisiana', abbreviation: 'LA'},
-    {name: 'Maine', abbreviation: 'ME'},
-    {name: 'Marshall Islands', abbreviation: 'MH'},
-    {name: 'Maryland', abbreviation: 'MD'},
-    {name: 'Massachusetts', abbreviation: 'MA'},
-    {name: 'Michigan', abbreviation: 'MI'},
-    {name: 'Minnesota', abbreviation: 'MN'},
-    {name: 'Mississippi', abbreviation: 'MS'},
-    {name: 'Missouri', abbreviation: 'MO'},
-    {name: 'Montana', abbreviation: 'MT'},
-    {name: 'Nebraska', abbreviation: 'NE'},
-    {name: 'Nevada', abbreviation: 'NV'},
-    {name: 'New Hampshire', abbreviation: 'NH'},
-    {name: 'New Jersey', abbreviation: 'NJ'},
-    {name: 'New Mexico', abbreviation: 'NM'},
-    {name: 'New York', abbreviation: 'NY'},
-    {name: 'North Carolina', abbreviation: 'NC'},
-    {name: 'North Dakota', abbreviation: 'ND'},
-    {name: 'Northern Mariana Islands', abbreviation: 'MP'},
-    {name: 'Ohio', abbreviation: 'OH'},
-    {name: 'Oklahoma', abbreviation: 'OK'},
-    {name: 'Oregon', abbreviation: 'OR'},
-    {name: 'Palau', abbreviation: 'PW'},
-    {name: 'Pennsylvania', abbreviation: 'PA'},
-    {name: 'Puerto Rico', abbreviation: 'PR'},
-    {name: 'Rhode Island', abbreviation: 'RI'},
-    {name: 'South Carolina', abbreviation: 'SC'},
-    {name: 'South Dakota', abbreviation: 'SD'},
-    {name: 'Tennessee', abbreviation: 'TN'},
-    {name: 'Texas', abbreviation: 'TX'},
-    {name: 'Utah', abbreviation: 'UT'},
-    {name: 'Vermont', abbreviation: 'VT'},
-    {name: 'Virgin Islands', abbreviation: 'VI'},
-    {name: 'Virginia', abbreviation: 'VA'},
-    {name: 'Washington', abbreviation: 'WA'},
-    {name: 'West Virginia', abbreviation: 'WV'},
-    {name: 'Wisconsin', abbreviation: 'WI'},
-    {name: 'Wyoming', abbreviation: 'WY'}
-  ];
-
-  constructor(private fb: FormBuilder) {}
-
-  onSubmit(): void {
-    alert('Thanks!');
+  ngOnInit() {
+    this.addressForm = EnhancedFormGroup.create({
+      lvaName: new EnhancedFormControl<string>('', Validators.required),
+      lvaTeacher: new EnhancedFormControl<string>('', Validators.required),
+      lvaNumber: new EnhancedFormControl<number>('', Validators.required),
+      lvaType: new EnhancedFormControl<number>('', Validators.required),
+      lvaRoom: new EnhancedFormControl<string>('', Validators.required),
+      lvaInstitute: new EnhancedFormControl<string>('', Validators.required),
+      lvaExam: new EnhancedFormControl<Date | null>(),
+      lvaEcts: new EnhancedFormControl<number>('', Validators.required),
+    })
   }
+  onSubmit(): void {
+    const fv = this.addressForm.value;
+    console.log(fv.lvaName);
+    this.lva.lvaName = fv.lvaName;
+    this.lva.lvaTeacher = fv.lvaTeacher;
+    this.lva.lvaNumber = fv.lvaNumber;
+    this.lva.lvaType = fv.lvaType;
+    this.lva.lvaRoom = fv.lvaRoom;
+    this.lva.lvaInstitute = fv.lvaInstitute;
+    this.lva.lvaExam = fv.lvaExam;
+    this.lva.lvaEcts = fv.lvaEcts;
+  
+    this._lvaService.createLva(this.lva).subscribe(i => {
+      alert('Thanks!');
+      console.log("LVA Created!");
+    });
+  }
+
+
 }
